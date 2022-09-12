849a134b4717c2ed4c50515dc82df2e4c43730d9
raulriato <raulriato@gmail.com>
2022-09-12 10:36:52 -0300

    feat post signin

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
 import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 const server = express();
@@ -53,6 +54,53 @@ server.post("/sign-up", async (req, res) => {
         res.status(201).send("Cadastrado com sucesso");
     } catch (error) {
         console.error(error)
+    }
+});
+
+server.post("/sign-in", async (req, res) => {
+    const { email, password } = req.body;
+
+    const signInSchema = joi.object({
+        email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+        password: joi.string().required()
+    });
+
+    const signInValidation = signInSchema.validate(req.body, {abortEarly: false});
+    
+    if (signInValidation.error) {
+        const messages = signInValidation.error.details.map(detail => detail.message);
+        res.status(422).send(messages);
+        return;
+    }
+
+    try {
+        const user = await db.collection("users").findOne({ email });
+        const session = await db.collection("sessions").findOne({ email });
+
+        const isValidPassword = bcrypt.compareSync(password, user.password);
+
+        if (!isValidPassword) {
+            return res.sendStatus(401);
+        }
+
+        if (session) {
+            await db.collection("sessions").deleteOne(session)
+        }
+
+        const token = uuidv4();
+
+        db.collection("sessions").insertOne({
+            token,
+            userId: user._id
+        });
+
+        res.status(201).send({
+            email,
+            token,
+            name: user.name
+        })
+    } catch (error) {
+        console.error(error);
     }
 });
 
